3e09e33dcb25fcc0bdf6b7997ee3ba5287c0d14f
Lars-Magnus Skog <ralphtheninja@riseup.net>
2021-06-14 14:24:02 +0200

    exit test script after publish

--- test-door-open-mqtt.js
+++ test-door-open-mqtt.js
@@ -2,5 +2,7 @@ import createMqttClient from './mqtt.js'
 const mqtt = createMqttClient()
 
 setTimeout(() => {
-  mqtt.publish('door/open', JSON.stringify({ foo: 'bar' }))
+  mqtt.publish('door/open', JSON.stringify({ foo: 'bar' }), () => {
+    process.exit(0)
+  })
 }, 2000)
